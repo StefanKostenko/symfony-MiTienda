70815da0eee386bf425179a21233c7b564963111
StefanKostenko <kostenkostephan@gmail.com>
2022-12-19 13:09:00 +0100

    html pasados a twig falta hacer partials

--- symfony-miTienda/src/Controller/PageController.php
+++ symfony-miTienda/src/Controller/PageController.php
@@ -8,11 +8,58 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class PageController extends AbstractController
 {
-    #[Route('/page', name: 'app_page')]
+    #[Route('/', name: 'index')]
     public function index(): Response
     {
-        return $this->render('page/index.html.twig', [
-            'controller_name' => 'PageController',
-        ]);
+        return $this->render('page/index.html.twig', []);
+    }
+
+    #[Route('/checkout', name: 'checkout')]
+    public function checkout(): Response
+    {
+        return $this->render('page/checkout.html.twig', []);
+    }
+
+    #[Route('/contatct', name: 'contatct')]
+    public function contatct(): Response
+    {
+        return $this->render('page/contatct.html.twig', []);
+    }
+
+    #[Route('/experiance', name: 'experiance')]
+    public function experiance(): Response
+    {
+        return $this->render('page/experiance.html.twig', []);
+    }
+
+    #[Route('/login', name: 'login')]
+    public function login(): Response
+    {
+        return $this->render('page/login.html.twig', []);
+    }
+
+    #[Route('/register', name: 'register')]
+    public function register(): Response
+    {
+        return $this->render('page/register.html.twig', []);
+    }
+
+    #[Route('/shop', name: 'shop')]
+    public function shop(): Response
+    {
+        return $this->render('page/shop.html.twig', []);
+    }
+
+    #[Route('/single', name: 'single')]
+    public function single(): Response
+    {
+        return $this->render('page/single.html.twig', []);
+    }
+
+    #[Route('/team', name: 'team')]
+    public function team(): Response
+    {
+        return $this->render('page/team.html.twig', []);
     }
 }
+
